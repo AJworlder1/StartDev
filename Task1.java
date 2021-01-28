eb95c85f0a8dee7a77df332bbcb18c4330c1113e
Your Name <you@example.com>
2021-01-28 14:20:15 +0200

    asd

---
+++ Task1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package startdev;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Task1 {
+    public void run(){
+        System.out.println("----- Задача 1 ------");
+        int a;
+        int d = 32;
+        double c = 0;
+        double b = 1.8;
+        System.out.println("Введите число градусов по Цельсию: ");
+        a = new Scanner(System.in).nextInt(); 
+        c = a * b + d;
+        System.out.println("По Фарeнгейту это будет: " + c);
+        System.out.println("----- конец задачи 1 ------");
+    }
+}
